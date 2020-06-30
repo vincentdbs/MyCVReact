f28ab7b14d4c4422ce8564a52ae60b196e3c8646
vincent_dubois_efrei_net <vincent.dubois@efrei.net>
2020-06-30 18:08:08 +0200

    refactor svg

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, {Component} from 'react';
 import './App.css';
+import ContainerInfo from "./Ressources/Component/ContainerInfo";
 
 class App extends Component{
-    
-
     render() {
         return(
-          <div>
-              <h1>fdsf</h1>
-          </div>
+          <ContainerInfo />
         );
     }
 }
--- src/Ressources/Component/ContainerInfo.js
+++ src/Ressources/Component/ContainerInfo.js
@@ -1,9 +1,12 @@
 import React from "react";
+import AboutMe from "./AboutMe";
+import ContactMe from "./ContactMe";
 
 const ContainerInfo = () => {
     return(
       <div className="container-top info container" id="image-top">
-
+          <AboutMe/>
+          <ContactMe/>
       </div>
     );
 };
