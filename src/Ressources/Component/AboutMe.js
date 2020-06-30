56c0249572b51beb82beb1b77532c9204f149add
vincent_dubois_efrei_net <vincent.dubois@efrei.net>
2020-06-30 17:26:06 +0200

    setup + json + about me

---
+++ src/Ressources/Component/AboutMe.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AboutMe = () => {
+    return(
+        <div className="me">
+            <h1>Vincent DUBOIS</h1>
+            <p>M1 Software Engineering</p>
+            <p>EFREI Paris</p>
+        </div>
+    );
+};
+
+export default AboutMe;
---
+++ src/Ressources/Component/ContainerInfo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ContainerInfo = () => {
+    return(
+      <div className="container-top info container" id="image-top">
+
+      </div>
+    );
+};
+
+export default ContainerInfo
