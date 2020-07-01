ef3d1f682f1a13af66959b4a399ec699eca8a8b3
vincent_dubois_efrei_net <vincent.dubois@efrei.net>
2020-07-01 11:02:37 +0200

    education

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import ContainerInfo from "./Ressources/Component/ContainerInfo";
 import "../src/Ressources/CSS/All.css"
 import "../src/Ressources/CSS/Top.css"
+import "../src/Ressources/CSS/EducationalBackground.css"
+import ContainerEducation from "./Ressources/Component/ContainerEducation";
 
-class App extends Component{
-    render() {
+class App extends Component{    render() {
         return(
-          <ContainerInfo />
+          <ContainerEducation />
         );
     }
 }
