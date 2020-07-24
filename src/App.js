cb455700a58517ab4eace2d54489ed55b3ba73c7
vincent_dubois_efrei_net <vincent.dubois@efrei.net>
2020-07-24 10:18:19 +0200

    skills css

--- src/App.js
+++ src/App.js
@@ -276,7 +276,6 @@ class App extends Component{
         // todo icon dans l'onglet
         //todo description dans lien à changer (voir mp nathan
         //todo skills cassé à 1400px
-        //todo no justify for education card description
         //todo centrer verticalement la timeline education
     }
 }
