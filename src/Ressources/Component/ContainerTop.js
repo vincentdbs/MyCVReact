e1331b9ee4788c75730432363ba89a9b4f4d2535
vincent_dubois_efrei_net <vincent.dubois@efrei.net>
2020-09-19 17:41:44 +0200

    centering div

--- src/Ressources/Component/ContainerTop.js
+++ src/Ressources/Component/ContainerTop.js
@@ -6,9 +6,11 @@ import ContactMe from "./ContactMe";
 const ContainerTop = () => {
     return (
         <div className="container-top info container" id="image-top">
-            <div>
-                <AboutMe/>
-                <ContactMe/>
+            <div className="container-top-left">
+                <div className="wrapper-me">
+                    <AboutMe/>
+                    <ContactMe/>
+                </div>
             </div>
             <div className="container-top-right">
                 <img src={require("../Image/flat_programming.png")} alt={"logo"}/>
