704c45f4f65d672a03689552a45eb3e64a2b4d85
vincent_dubois_efrei_net <vincent.dubois@efrei.net>
2020-09-19 18:57:57 +0200

    add arrow down

--- src/Ressources/Component/ContainerTop.js
+++ src/Ressources/Component/ContainerTop.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "../CSS/ContainerInfo.css"
 import AboutMe from "./AboutMe";
 import ContactMe from "./ContactMe";
+import {svgPath} from "../Logo/svgPath";
+import SVG from "./SVG";
 
 const ContainerTop = () => {
+    useEffect(() => {
+            var info = document.querySelector('.container-arrowdown');
+            info.addEventListener('click', function () {
+                document.querySelector('.education').scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+        }
+    );
+
     return (
         <div className="container-top info container" id="image-top">
             <div className="container-top-left">
@@ -14,6 +26,9 @@ const ContainerTop = () => {
             </div>
             <div className="container-top-right">
                 <img src={require("../Image/flat_programming.png")} alt={"logo"}/>
+            </div>
+            <div className="center container-arrowdown">
+                <SVG className={"arrowdonw"} path={svgPath.arrowdown} />
             </div>
         </div>
     );
