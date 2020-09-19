6276c9029dabb16e77633444741bf030ebf305d9
vincent_dubois_efrei_net <vincent.dubois@efrei.net>
2020-09-19 16:56:29 +0200

    setup bg

---
+++ src/Ressources/Component/ContainerTop.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../CSS/ContainerInfo.css"
+import AboutMe from "./AboutMe";
+import ContactMe from "./ContactMe";
+
+const ContainerTop = () => {
+    return (
+        <div className="container-top info container" id="image-top">
+            <div>
+                <AboutMe/>
+                <ContactMe/>
+            </div>
+            <div className="container-top-right">
+                <img src={require("../Image/flat_programming.png")} alt={"logo"}/>
+            </div>
+        </div>
+    );
+};
+
+
+export default ContainerTop;
