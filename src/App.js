a58d69edf47529ecf87dd459d542f9901571fb32
vincent_dubois_efrei_net <vincent.dubois@efrei.net>
2020-09-21 11:46:46 +0200

    error 404

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
 import React, {Component} from 'react';
 import './App.css';
 import "../src/Ressources/CSS/All.css"
-import "../src/Ressources/CSS/Scrollbar.css"
 
-import {LanguageContext, dictionaryList} from './Ressources/Language/LanguageContext';
-import ContainerEducation from "./Ressources/Component/Container/ContainerEducation";
-import ContainerExperience from "./Ressources/Component/Container/ContainerExperience";
-import ContainerProject from "./Ressources/Component/Container/ContainerProject";
-import ContainerSkills from "./Ressources/Component/Container/ContainerSkills";
-import BG_dark from "../src/Ressources/Image/bg_header_dark.jpg";
-import BG_light from "./Ressources/Image/bg_header-light.jpg";
-import Footer from "./Ressources/Component/Footer";
-import {svgPath} from "./Ressources/Logo/svgPath";
-import Navigation from "./Ressources/Component/Navigation";
-import ContainerTop from "./Ressources/Component/Container/ContainerTop";
-import { Redirect, Route, Switch, BrowserRouter } from 'react-router-dom';
+import {Route, Switch, BrowserRouter } from 'react-router-dom';
 import Error404 from "./Ressources/Component/Error404";
 import LandingPage from "./Ressources/Component/LandingPage";
 
@@ -29,7 +17,6 @@ class App extends Component{
             </BrowserRouter>
     );
         // todo icon dans l'onglet
-        // todo error 404
     }
 }
 
--- src/Ressources/Component/Error404.js
+++ src/Ressources/Component/Error404.js
@@ -1,9 +1,15 @@
 import React from "react";
+import "../CSS/Container/ContainerError404.css"
 
 const Error404 = () => {
     return (
-        <div>
-            <p>Error 404</p>
+        <div className="container container-404">
+            <div className="div-error404">
+                <h1 className="title-error404">Error 404</h1>
+                <img src={require("../Image/error404.png")} />
+                <p>Sorry page not found</p>
+                <a href="http://www.vincentdbs.fr">Go back to homepage</a>
+            </div>
         </div>
     );
 };
