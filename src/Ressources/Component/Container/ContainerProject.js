150a55c3216cfaf01113e5b5b2867f31e45ceaea
vincent_dubois_efrei_net <vincent.dubois@efrei.net>
2020-09-26 17:54:30 +0200

    logo

--- src/Ressources/Component/Container/ContainerProject.js
+++ src/Ressources/Component/Container/ContainerProject.js
@@ -7,7 +7,8 @@ const ContainerProject = () => {
     let language = useContext(LanguageContext);
     return (
         <div className="container project">
-            <svg className="project-wave-top" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ff8a11" fillOpacity="1" d="M0,96L40,96C80,96,160,96,240,122.7C320,149,400,203,480,202.7C560,203,640,149,720,112C800,75,880,53,960,80C1040,107,1120,181,1200,208C1280,235,1360,213,1400,202.7L1440,192L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg><div className="project-body">
+            <svg className="project-wave-top" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ff8a11" fillOpacity="1" d="M0,96L40,96C80,96,160,96,240,122.7C320,149,400,203,480,202.7C560,203,640,149,720,112C800,75,880,53,960,80C1040,107,1120,181,1200,208C1280,235,1360,213,1400,202.7L1440,192L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
+            <div className="project-body">
                 <div className="center">
                     <h1 className="section-title">{language.project.title}</h1>
                 </div>
