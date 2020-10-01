4c2b71e8b1830a12f18560ff75b8a7be1c918d2f
vincent_dubois_efrei_net <vincent.dubois@efrei.net>
2020-10-01 14:59:26 +0200

    rename color variable

--- src/Ressources/Component/Education.js
+++ src/Ressources/Component/Education.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "../CSS/Education.css"
+import "../CSS/CardEducation.css"
 
 const Education = ({date, address, title, tasks, cat}) => {
     return (
